853d3af1fa7a9e98c7c3bdbbda281435b4b9a80f
Frank-Kam <ganl68384@gmail.com>
2025-05-09 23:43:53 +0800

    edit robots

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,27 +2,12 @@
 import type { APIRoute } from 'astro'
 
 const robotsTxt = `
-User-agent: Googlebot
-Disallow: /friends*
-Disallow: /posts/school-sec*
-Allow: /
-Crawl-delay: 10
-
-User-agent: Yandex
-Disallow: /friends*
-Disallow: /posts/school-sec*Allow: /
-Crawl-delay: 2
-
-User-agent: archive.org_bot
-Disallow: /friends*
-Disallow: /posts/school-sec*
-Allow: /
-Crawl-delay: 2
-
 User-agent: *
 Allow: /
 Disallow: /friends*
 Disallow: /posts/school-sec*
+Disallow: /vendor*
+Disallow: /search*
 Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}`.trim()
 
 export const GET: APIRoute = () => {
