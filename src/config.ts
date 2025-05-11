cd3f519a2bea927aba5e5205e8a36dccf3b45a95
Frank-Kam <ganl68384@gmail.com>
2025-05-11 22:40:48 +0800

    edit categories name

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const SITE: Site = {
 
 export const NavigationLinks: NavigationLink[] = [
     { name: 'Posts', url: '/posts' },
-    { name: 'Categories', url: '/categories' },
+    { name: 'Category', url: '/categories' },
     { name: 'Timeline', url: '/timeline' },
     { name: 'About', url: '/posts/about' },
     { name: 'Friends', url: '/friends' },
