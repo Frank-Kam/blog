941d451780f0a48c2363d6fb4dde40500aa1967f
Frank-Kam <ganl68384@gmail.com>
2025-06-14 02:50:35 +0800

    feat(config): disable Astro version meta tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import { uploadAssetsToS3 } from './src/support/uploader.ts'
 
 export default defineConfig({
     site: SITE.url,
+    injectGenerator: false,
     image: {
         // If you don't want to optimize images during the BUILD process please set the ASTRO_IMAGE_OPTIMIZE environment variable to false
         // Please note that the environment value here is `string` type on Cloudflare Pages,
