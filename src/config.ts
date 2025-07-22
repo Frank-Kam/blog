d45b1c7089d9f4346fbd977bba20a2e78326db2a
Frank-Kam <ganl68384@gmail.com>
2025-07-22 20:55:42 +0800

    chore(pnpm): update dependencies

--- src/config.ts
+++ src/config.ts
@@ -68,8 +68,8 @@ export const Settings = {
         // If you don't want to upload the build assert(image/js/css/etc...) to anywhere, just set this to false
         // Please note that the environment value here is `string` type on Cloudflare Pages
         // If you want to disable the comment system, please delete the `S3_ENABLE` environment variable not just set it to `false`.
-        uploadAssetsToS3: !!(import.meta.env.S3_ENABLE) || !!process.env.S3_ENABLE,
-        config: {
+        //uploadAssetsToS3: !!(import.meta.env.S3_ENABLE) || !!process.env.S3_ENABLE,
+        //config: {
             // see https://github.com/syhily/astro-uploader to get how to configure the uploader,
             // The following configuration will upload the compiled `assets` folder to S3 or R2.
             // You can set a separate domain for it so that you can access all resources using a CDN domain name.
@@ -78,13 +78,13 @@ export const Settings = {
             //
             // Note that you may also need to modify `build.assetsPrefix` in `astro.config.mjs` if you want to
             // automatically replace all images/js/css with a CDN link.
-            paths: ['assets'],
-            endpoint: (process.env.S3_ENDPOINT ?? import.meta.env.S3_ENDPOINT) as string,
-            bucket: (process.env.S3_BUCKET ?? import.meta.env.S3_BUCKET) as string,
-            accessKey: (process.env.S3_ACCESS_KEY ?? import.meta.env.S3_ACCESS_KEY) as string,
-            secretAccessKey: (process.env.S3_SECRET_ACCESS_KEY ?? import.meta.env.S3_SECRET_ACCESS_KEY) as string,
-            root: 'gblog',
-        },
+            //paths: ['assets'],
+            //endpoint: (process.env.S3_ENDPOINT ?? import.meta.env.S3_ENDPOINT) as string,
+            //bucket: (process.env.S3_BUCKET ?? import.meta.env.S3_BUCKET) as string,
+            //accessKey: (process.env.S3_ACCESS_KEY ?? import.meta.env.S3_ACCESS_KEY) as string,
+            //secretAccessKey: (process.env.S3_SECRET_ACCESS_KEY ?? import.meta.env.S3_SECRET_ACCESS_KEY) as string,
+            //root: 'gblog',
+        //},
     },
 }
 
